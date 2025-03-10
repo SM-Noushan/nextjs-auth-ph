db55f661474c9c5fbf5e45d4acc64aa110405403
SMNoushan <noushan.md@gmail.com>
2025-03-10 12:46:35 +0600

    add: github login

--- src/app/utils/authOptions.ts
+++ src/app/utils/authOptions.ts
@@ -1,11 +1,11 @@
 import { NextAuthOptions } from "next-auth";
-// import GithubProvider from "next-auth/providers/github";
+import GitHubProvider from "next-auth/providers/github";
 
 export const authOptions: NextAuthOptions = {
   providers: [
-    // GithubProvider({
-    //   clientId: process.env.GITHUB_ID,
-    //   clientSecret: process.env.GITHUB_SECRET,
-    // }),
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
   ],
 };
